eee88594ae811b1ff03f318550af93c915f08832
Quantum-Code-lab <amirkhalifa@zoho.com>
2021-08-29 15:01:55 +0600

    first commit

---
+++ findDomain.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="style.css">
+<form>
+    <input type="text" name="domain" id="domain">
+    <input type="button" value="Click" onclick="checkDomain()">
+    <div id="result"></div>
+</form>
+<script src="jquery-3.6.0.min.js"></script>
+<script>
+    function checkDomain()
+    {
+        //jQuery('#result').html('Domain');
+        jQuery('#result').html(' ');
+        var domain=jQuery('#domain').val();
+        if (domain=='')
+        {
+            jQuery('#result').html('Please Enter Your Domain');
+        }
+        else
+        {
+            jQuery('#result').html('<img src="328.gif"/>');
+
+            jQuery.ajax({
+                 url:'https://domain-availability.whoisxmlapi.com/api/v1?apiKey=at_DhzLdu38u61zrjVhg0YwmGZmsFaiw&domainName='+domain,
+                 success:function(result)
+                 {
+                    jQuery('#result').html('Your Domain Name is :&nbsp;&nbsp;'+result.DomainInfo.domainAvailability);
+                     
+                 }
+            });
+        }
+    }
+</script>
